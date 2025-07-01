11aa66a3bd62259bb60f9405b9eb461fb9b398b9
Kessoku-Archive <159677941+Kessoku-Archive@users.noreply.github.com>
2025-07-01 15:26:58 +0900

    드디어첫커밋끼얏호우git add .!

---
+++ JsonTest/Character.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+[System.Serializable]
+
+    public class Character
+    {
+    public string name { get; set; }
+    public int CurrentHP { get; set; }
+    public int MaxHP { get; set; }
+    //여러 캐릭터를 다루려면 리스트로
+}
---
+++ JsonTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json; // 도구 << nuget
+namespace JsonTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "characters.json";
+
+            List<Character>? characters = null;
+            try
+            {
+                string json = File.ReadAllText(path);
+                characters = JsonConvert.DeserializeObject<List<Character>>(json);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("에러: " + ex.Message);
+            }
+
+            if (characters == null)
+            {
+
+                Console.WriteLine("데이터가 없습니다");
+                return;
+            }
+                foreach (var c in characters)
+                {
+                    Console.WriteLine($"이름: {c.name}, HP: {c.CurrentHP} / {c.MaxHP}");
+                }
+            
+        }
+    }
+}
